a4fefa6f7cc65fd40544698c91fd5ac479ef0d39
inesoitzinger <116837209+inesoitzinger@users.noreply.github.com>
2025-11-05 00:54:37 +0100

    test: add initial integration test for DB fetch

--- backend/src/test/java/com/sportradar/inesoitzinger/BackendApplicationTests.java
+++ backend/src/test/java/com/sportradar/inesoitzinger/BackendApplicationTests.java
@@ -1,13 +1,70 @@
-//package com.sportradar.inesoitzinger;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class BackendApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.sportradar.inesoitzinger;
+
+import com.sportradar.inesoitzinger.repositories.MatchRepository;
+import com.sportradar.inesoitzinger.repositories.SportRepository;
+import com.sportradar.inesoitzinger.repositories.TeamRepository;
+import com.sportradar.inesoitzinger.repositories.VenueRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Autowired
+    SportRepository sportRepository;
+
+    @Autowired
+    TeamRepository teamRepository;
+
+    @Autowired
+    VenueRepository venueRepository;
+
+    @Autowired
+    MatchRepository matchRepository;
+
+    @Test
+    void can_fetch_sports() {
+        var sports = sportRepository.findAll();
+        assertThat(sports).isNotEmpty();
+
+        sports.forEach(s ->
+                System.out.println(s.getId() + " - " + s.getName())
+        );
+    }
+
+    @Test
+    void can_fetch_teams() {
+        var teams = teamRepository.findAll();
+        assertThat(teams).isNotEmpty();
+
+        teams.forEach(t ->
+                System.out.println(t.getId() + " - " + t.getName())
+        );
+    }
+
+    @Test
+    void can_fetch_venues() {
+        var venues = venueRepository.findAll();
+        assertThat(venues).isNotEmpty();
+
+        venues.forEach(v ->
+                System.out.println(v.getId() + " - " + v.getName())
+        );
+    }
+
+    @Test
+    void can_fetch_matches() {
+        var matches = matchRepository.findAll();
+        assertThat(matches).isNotEmpty();
+
+        matches.forEach(m ->
+                System.out.println(m.getId()
+                        + " - " + m.getHomeTeam().getName()
+                        + " - " + m.getAwayTeam().getName()
+        ));
+    }
+
+}
